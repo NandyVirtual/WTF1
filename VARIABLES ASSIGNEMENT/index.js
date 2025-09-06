de3120a57f4451411cfa5024591cffa65b5f8073
unknown <joanvitual256@outlook.com>
2025-09-06 15:13:19 +0200

    Completed assignment

---
+++ VARIABLES ASSIGNEMENT/index.js
@@ -0,0 +1,19 @@
+// Declare variables
+let num1 = 10;
+let num2 = 5;
+let name = "Joan";
+
+// Perform operations
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+
+// Print results
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+
+// Print personalized message
+console.log(`Hello ${name}, the result of num1 + num2 is ${addition}`);
